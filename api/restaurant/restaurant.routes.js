03dfa19fad4f671ad2163817904df638b9b21704
EyalAlfasiSkills <eyala@academy-skills.com>
2021-08-01 11:40:48 +0300

    .

--- api/restaurant/restaurant.routes.js
+++ api/restaurant/restaurant.routes.js
@@ -11,7 +11,7 @@ const {
 
 const router = express.Router();
 
-router.get("/:restaurantId?", validateCognitoAccessToken, getRestaurants);
+router.get("/:restaurantId?", getRestaurants);
 
 router.post("/", validateCognitoAccessToken, saveRestaurant);
 
